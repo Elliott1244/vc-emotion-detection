b21c3e4c4f51e032702893a4a5f13d2bbe3abd71
HarshaKiranAdapala <harshakiranadapala7@gmail.com>
2025-06-29 14:11:25 +0100

    Add cookircutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='AN ML project that detects human emotions',
+    author='Harshakiran',
+    license='',
+)
